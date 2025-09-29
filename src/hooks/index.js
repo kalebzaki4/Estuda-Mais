3079912f954891275ea5d31ae8dea82d06f49ccc
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-09-29 10:48:20 -0300

    RECOMEÇANDO O PROJETO...

--- src/hooks/index.js
+++
@@ -1,9 +0,0 @@
-// Exportações dos hooks customizados
-export { default as useLocalStorage } from './useLocalStorage';
-export { default as useForm } from './useForm';
-
-// Hooks que serão criados posteriormente
-// export { default as useDebounce } from './useDebounce';
-// export { default as useApi } from './useApi';
-// export { default as useToggle } from './useToggle';
-// export { default as usePrevious } from './usePrevious';
