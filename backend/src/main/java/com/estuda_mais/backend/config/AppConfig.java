1671be4aa4b9776d8e1d0496790b526a5419a59f
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-11-10 18:29:45 -0300

    Create AppConfig.java

---
+++ backend/src/main/java/com/estuda_mais/backend/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.estuda_mais.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+}
