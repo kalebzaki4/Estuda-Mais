bab95b8c5cc7989e80222b60ff629709b662dd1e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-01-13 15:47:48 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Estuda-Mais/', 
   plugins: [react({
       babel: {
         plugins: ['styled-jsx/babel']
