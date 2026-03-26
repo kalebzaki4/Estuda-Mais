366aa3d9a89498b36c3a540fca22574e73c70de2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-03-26 18:19:41 -0300

    fix: removendo node_modules do rastreio

---
+++ api/src/main/java/com/estudamais/api/controller/AutenticationController.java
@@ -0,0 +1,31 @@
+package com.estudamais.api.controller;
+
+import com.estudamais.api.dto.AutenticacaoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/login")
+public class AutenticationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AutenticationController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestBody AutenticacaoDTO dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.nome(), dados.senha());
+        var authentication = authenticationManager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+}
